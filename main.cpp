74278d04bacec4d1fc0df8e510b41ddecda9b282
Mattiusw <mattiusw@gmail.com>
2025-08-26 11:39:14 +0100

    Make a base main file to load every algorithm

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "src/1929_Concatenation_of_Array.cpp"
+#include<iostream>
+
+int main(void) {
+
+    // Riddle solution - 1929 Concatenation of Array
+    vector<int> nums = { 1, 2, 1 };
+    vector<int> full_ans = getConcatenation(nums);
+    for (auto i : full_ans) cout << i << " ";
+    
+}

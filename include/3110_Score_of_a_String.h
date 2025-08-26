54a95fcb2a8c733463505d82e0486e8267134715
Mattiusw <mattiusw@gmail.com>
2025-08-26 14:30:50 +0100

    Revert "Set Cmake to auto find files"
    This reverts commit c0574c923bfc1583ea4cd759596f9052b3173daa.

--- include/3110_Score_of_a_String.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include<string>
-#include<cmath>
-
-int scoreOfString(std::string s);
--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "src/1929_Concatenation_of_Array.cpp"
-#include "src/3110_Score_of_a_String.cpp"
 #include<iostream>
 
 int main(void) {
@@ -7,15 +6,6 @@ int main(void) {
     // Riddle solution - 1929 Concatenation of Array
     vector<int> nums = { 1, 2, 1 };
     vector<int> full_ans = getConcatenation(nums);
-    cout << "Original nums: ";
-    for (auto i : nums) cout << i << " ";
-    cout << " Concatenation nums: ";
-    for (auto j : full_ans) cout << j << " ";
-    cout << endl;
-
-    // Riddle solution - 3110 Concatenation of Array
-    string s1 = "hello";
-    int ans1 = scoreOfString(s1);
-    cout << "String: " << s1 << " == "<< ans1 << endl;
+    for (auto i : full_ans) cout << i << " ";
     
 }

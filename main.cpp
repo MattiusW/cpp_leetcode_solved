648ba31a01d3ec23c61820f2de1e9d691afa4047
Mattiusw <mattiusw@gmail.com>
2025-08-27 12:29:56 +0100

    Show 3467 solution in main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "src/1929_Concatenation_of_Array.cpp"
 #include "src/3110_Score_of_a_String.cpp"
+#include "src/3467_Transform_Array_by_Parity.cpp"
 #include<iostream>
 
 int main(void) {
@@ -18,4 +19,8 @@ int main(void) {
     int ans1 = scoreOfString(s1);
     cout << "String: " << s1 << " == "<< ans1 << endl;
     
+    //Riddle solution - 3467 Transform Array by Parity
+    vector<int> nums3467 = {4,3,2,1};
+    vector<int> ans = transformArray(nums3467);
+    for (auto i : ans) std::cout << i << " ";
 }
